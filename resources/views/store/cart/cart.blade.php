e2544d304f681a1d2af283d28abb81ea289f9abf
AlvaroFerreiraAlves <alvarof.alves@hotmail.com>
2017-12-29 17:33:01 -0300

    cart add remove

--- resources/views/store/cart/cart.blade.php
+++ resources/views/store/cart/cart.blade.php
@@ -10,18 +10,28 @@
             <th>Quantidade</th>
             <th>Sub. total</th>
         </tr>
-
+        @for($i=0;$i<8;$i++)
         <tr>
             <td>
-                <img src="{{url('assets/imgs/temp/tv.jpg')}}" alt="">
+                <img src="{{url('assets/imgs/temp/tv.jpg')}}" alt="" class="img-cart">
                 Nome do produto
             </td>
             <td>R$ 200.00</td>
-            <td>2</td>
+            <td>
+                2
+                <a href="" class="cart-action-item">+</a> -
+                <a href="" class="cart-action-item">-</a>
+            </td>
             <td>400,00</td>
         </tr>
+            @endfor
     </table>
 
+    <div class="total-cart"><span>R$ 200,00</span></div>
+
+    <div class="finish-cart">
+        <a href="">Finalizar compra <i class="fa fa-shopping-cart" aria-hidden="true"></i> </a>
+    </div>
 
 
 @endsection
